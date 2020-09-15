9414165fa12298bb389f5cf1de7993e7b3dee9cf
Subu Kaundinya <subu.kaundinya@gmail.com>
2020-09-15 11:52:38 -0400

    greek alphabet teaching

---
+++ main.py
@@ -0,0 +1,105 @@
+import greekalpha as ga
+import greekalpha.alphabet as alphabet
+from alphabet import GREEKALPHABET
+
+#Here is where I teach you about the greek alphabet
+True
+#alpha
+def alpha 
+def alpha-equivalent='a'
+return Αα
+#beta
+def beta
+def beta-equivalent='b'
+return Ββ
+#gamma
+def gamma
+def gamma-equivalent='g'
+return Γγ
+#delta
+def delta
+def delta-equivalent='d'
+return Δδ
+#epsilon
+def epsilon
+def epsilon-equivalent='e'
+return Εε
+#zeta
+def zeta
+def zeta-equivalent='z'
+return Ζζ
+#eta
+def eta
+def eta-equivalent='h'
+return Ηη
+#theta
+def theta
+def theta-equivalent='th'
+return Θθ
+#iota
+def iota
+def iota-equivalent='i'
+return Ιι
+#kappa
+def kappa
+def kappa-equivalent='k'
+return Κκ
+#lambda
+def lambda
+def lambda-equivalent='l'
+return Λλ
+#mu
+def mu
+def mu-equivalent='m'
+return Μμ
+#nu
+def nu
+def nu-equivalent='n'
+return Νν	
+#xi
+def xi
+def xi-equivalent='x'
+return Ξξ
+#omicron
+def omicron
+def omicron-equivalent='o'
+return Οο
+#pi
+def pi
+def pi-equivalent='p'
+return Ππ
+#rho
+def rho
+def rho-equivalent='r'
+return Ρρ
+#sigma
+def sigma
+def sigma-equivalent='s'
+return Σσ,ς: used in word final position
+#tau
+def tau
+def tau-equivalent='t'
+return Ττ
+#upsilon
+def upsilon
+def upsilon-equivalent='u'
+return Υυ
+#phi
+def phi
+def phi-equivalent='ph'
+return Φφ
+#chi
+def chi
+def chi-equivalent='ch'
+return Χχ
+#psi
+def psi
+def psi-equivalent='ps'
+return Ψψ
+#omega
+def omega
+def omega-equivalent='o'
+return Ωω
+#irrational numbers
+π=3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955
+e=2.71828182845904523536028747135266249775724709369995957496696762772407663035354759457138217852516642742746639193200305992181741359662
